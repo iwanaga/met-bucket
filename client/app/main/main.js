fc3a8d897a9d9cece67eb81788984da10fd858bb
iwanaga <iwanaga@iij.ad.jp>
2015-01-18 17:21:54 +0900

    state composition for humidity

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,11 +1,19 @@
 'use strict';
 
-angular.module('metBucketApp')
-  .config(function($stateProvider) {
-    $stateProvider
-      .state('main', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
+angular.module('metBucketApp').config(function($stateProvider) {
+  $stateProvider.state('main', {
+    url: '/main',
+    templateUrl: 'app/main/main.html',
+    controller: 'MainCtrl'
+  }).state('main.home', {
+    url: '/home',
+    views: {
+      humidity: {
+        templateUrl: 'app/humidity/humidity.html',
+        controller: 'HumidityCtrl'
+      }
+    }
   });
+}).run(function ($state) {
+  $state.go('main.home');
+});
