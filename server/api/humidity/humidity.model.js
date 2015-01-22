89b05e3ee9b84850b3238d9ff06ced241d64fee0
iwanaga <iwanaga@iij.ad.jp>
2015-01-22 23:48:53 +0900

    revised Humidity model

--- server/api/humidity/humidity.model.js
+++ server/api/humidity/humidity.model.js
@@ -1,12 +1,21 @@
 'use strict';
 
 var mongoose = require('mongoose-bird')();
+var timestamp = require('mongoose-timestamp');
 var Schema = mongoose.Schema;
 
-var HumiditySchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
-});
+var HumiditySchema = new Schema(
+  {
+    value: Number
+  },
+  {
+    capped: {
+      size: 1073741824,
+      max: 1051200
+    }
+  }
+);
+HumiditySchema.plugin(timestamp);
+HumiditySchema.index({createdAt: -1});
 
 module.exports = mongoose.model('Humidity', HumiditySchema);
